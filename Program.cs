99224ed854ffe4e8638a71735a4b7e5442921d10
Dace Jansone <jansone-dace@inbox.lv>
2022-12-01 14:46:43 +0100

    day 1 part 2

--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine(String.Format("Day 1, part 1: {0}", Day1.Part1()));
-        Day1.Part1();
+        Console.WriteLine(String.Format("Day 1, part 2: {0}", Day1.Part2()));
     }
 }
--- Solutions/Day1.cs
+++ Solutions/Day1.cs
@@ -32,5 +32,47 @@ namespace Advent_of_Code_2022
 
             return maxCalories;
         }
+
+        /// <summary>
+        /// Find sum of total calories carried by top 3 elfs
+        /// </summary>
+        /// <returns></returns>
+        public static int Part2()
+        {
+            int currentCalories = 0;
+            int[] topCalories = new int[3];
+
+            foreach (string line in System.IO.File.ReadLines(@"Inputs/day1.txt"))
+            {  
+                if (String.IsNullOrWhiteSpace(line))
+                {
+                    switchCalories(topCalories, currentCalories);
+                    currentCalories = 0;
+                } 
+                else
+                    currentCalories += Int32.Parse(line);
+            }
+
+            switchCalories(topCalories, currentCalories);
+
+            return topCalories[0] + topCalories[1] + topCalories[2];
+        }
+
+        private static void switchCalories(int[] topCalories, int currentCalories)
+        {
+            if (currentCalories > topCalories[0])
+            {
+                topCalories[2] = topCalories[1];
+                topCalories[1] = topCalories[0];
+                topCalories[0] = currentCalories;
+            }
+            else if (currentCalories > topCalories[1])
+            {
+                topCalories[2] = topCalories[1];
+                topCalories[1] = currentCalories;
+            }
+            else if (currentCalories > topCalories[2])
+                topCalories[2] = currentCalories;
+        }
     }
 }
