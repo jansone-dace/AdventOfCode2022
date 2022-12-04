f56ec1743894bed73c06d9eca4c27767a58ca16a
jansone-dace <jansone-dace@inbox.lv>
2022-12-04 12:22:44 +0200

    Day 4, part 2

--- Program.cs
+++ Program.cs
@@ -12,5 +12,6 @@ class Program
         Console.WriteLine(String.Format("Day 3, part 1: {0}", Day3.Part1()));
         Console.WriteLine(String.Format("Day 3, part 2: {0}", Day3.Part2()));
         Console.WriteLine(String.Format("Day 4, part 1: {0}", Day4.Part1()));
+        Console.WriteLine(String.Format("Day 4, part 2: {0}", Day4.Part2()));
     }
 }
--- Solutions/Day4.cs
+++ Solutions/Day4.cs
@@ -24,5 +24,28 @@ namespace Advent_of_Code_2022.Solutions
 
             return overlappingAreas;
         }
+
+        public static int Part2()
+        {
+            int overlappingAreas = 0;
+            string firstPair, secondPair;
+            int firstPairFrom, firstPairTo, secondPairFrom, secondPairTo;
+
+            foreach (string line in System.IO.File.ReadLines(@"Inputs/day4.txt"))
+            {
+                firstPair = line.Split(',')[0];
+                secondPair = line.Split(',')[1];
+                firstPairFrom = Int32.Parse(firstPair.Split('-')[0]);
+                firstPairTo = Int32.Parse(firstPair.Split('-')[1]);
+                secondPairFrom = Int32.Parse(secondPair.Split('-')[0]);
+                secondPairTo = Int32.Parse(secondPair.Split('-')[1]);
+
+                if ((firstPairFrom <= secondPairFrom && firstPairTo >= secondPairFrom)
+                || (secondPairFrom <= firstPairFrom && secondPairTo >= firstPairFrom))
+                    overlappingAreas++;
+            }
+
+            return overlappingAreas;
+        }
     }
 }
