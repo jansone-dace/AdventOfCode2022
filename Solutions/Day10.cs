2df9ceb25800b966dffe8d40f261bde432e62ac9
Dace Jansone <jansone-dace@inbox.lv>
2022-12-10 12:41:57 +0100

    Day 10, part 2

--- Solutions/Day10.cs
+++ Solutions/Day10.cs
@@ -31,11 +31,52 @@ namespace Advent_of_Code_2022.Solutions
             return signalSum;
         }
 
+        public static string Part2()
+        {
+            string result = "";
+            int x = 1, currentCycle = 0, addx;
+
+            foreach (string line in System.IO.File.ReadLines(@"Inputs/day10.txt"))
+            {  
+                addx = 0;
+                if (line == "noop")
+                {
+                    result += draw(currentCycle, x);
+                    currentCycle++;
+                    continue;
+                }
+
+                addx = Int32.Parse(line.Split("addx ")[1]);
+                result += draw(currentCycle, x);
+                currentCycle++;
+                result += draw(currentCycle, x);
+                currentCycle++;
+                x += addx;
+            }
+
+            return result;
+        }
+
         private static int checkSignalStrength(int currentCycle, int x)
         {
             if (new[] {20,60,100,140,180,220}.Contains(currentCycle))
                 return currentCycle * x;
             return 0;
+        }
+
+        private static string draw(int currentCycle, int x)
+        {
+            string result;
+            int offset = currentCycle % 40;
+            if (offset == x || offset == x-1 || offset == x+1)
+                result = "#";
+            else
+                result = ".";
+            
+            if (offset == 0)
+                result += "\n";
+
+            return result;
         }
     }
 }
